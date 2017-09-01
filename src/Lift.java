8abce7abe2660138944ece77fa7bb4252cd4f7c6
tylox7 <rstolp@gmail.com>
2017-08-31 23:01:35 -0700

    First attempt at syncing a project between laptop and desktop.

--- src/Lift.java
+++ src/Lift.java
@@ -1,7 +1,58 @@
 public class Lift {
 
    // Variables
-   private int set;
-   private int rep;
+   private int sets;
+   private int reps;
 
+   private String liftName;
+   private String comments;
+
+   // Default Constructor
+   public Lift() {
+      liftName = "Default";
+      sets = 3;
+      reps = 10;
+      comments = "";
+   }
+
+   // Constructor that I'll mainly use for now.
+   public Lift(String liftName, int sets, int reps) {
+      this.liftName = liftName;
+      this.sets = sets;
+      this.reps = reps;
+      comments = "";
+   }
+
+   //Getters and Setters
+   public int getSets() {
+      return sets;
+   }
+
+   public void setSets(int sets) {
+      this.sets = sets;
+   }
+
+   public int getReps() {
+      return reps;
+   }
+
+   public void setReps(int reps) {
+      this.reps = reps;
+   }
+
+   public String getLiftName() {
+      return liftName;
+   }
+
+   public void setLiftName(String liftName) {
+      this.liftName = liftName;
+   }
+
+   public String getComments() {
+      return comments;
+   }
+
+   public void setComments(String comments) {
+      this.comments = comments;
+   }
 }
